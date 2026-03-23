e1e2a5167514faaff5611fe61dd44a5b93ba3680
Johan Larsson <johan@jolars.co>
2026-03-23 11:05:38 +0100

    chore: re-enable panache format pre-commit

--- devenv.nix
+++ devenv.nix
@@ -69,22 +69,22 @@
         enable = true;
       };
 
-      # panache-format = {
-      #   enable = true;
-      #
-      #   name = "panache format";
-      #
-      #   entry = "cargo run -- format";
-      #
-      #   language = "system";
-      #
-      #   files = "\.(qmd|md|Rmd)$";
-      #
-      #   excludes = [
-      #     "^(pandoc|assets|tests)"
-      #     "docs/user-guide/cli.qmd"
-      #   ];
-      # };
+      panache-format = {
+        enable = true;
+
+        name = "panache format";
+
+        entry = "cargo run --release -- format";
+
+        language = "system";
+
+        files = "\.(qmd|md|Rmd)$";
+
+        excludes = [
+          "^(pandoc|assets|tests)"
+          "docs/user-guide/cli.qmd"
+        ];
+      };
 
       eslint = {
         enable = true;
