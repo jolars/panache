a9339fb521d64d7b4f1b0c340993c0a6df208bab
Johan Larsson <johan@jolars.co>
2026-04-10 14:51:18 +0200

    chore: remove leftovers from parser refactor

--- src/parser/blocks/latex_envs.rs
+++
@@ -1,7 +0,0 @@
-//! Shared LaTeX environment parsing utilities.
-
-/// Information about a detected LaTeX environment opening.
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub(crate) struct LatexEnvInfo {
-    pub env_name: String,
-}
--- src/syntax/ast.rs
+++
@@ -1,4 +0,0 @@
-//! AST node trait and support utilities.
-
-pub(super) use rowan::ast::support;
-pub use rowan::ast::{AstChildren, AstNode};
